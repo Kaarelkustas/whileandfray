1f591abae61b921cc881ae527d5f599b11c1150a
Opilane TTHK <opilane@tthk.ee>
2020-09-28 14:56:04 +0300

    first commit

---
+++ GuessMyColor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessMyColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
